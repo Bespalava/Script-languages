9bac78f97ac83287bb5dd9586539384bfc05cf22
ybespalava <ybespalava@gmail.com>
2023-12-02 13:32:58 +0100

    initial commit

---
+++ tic_tac_toe.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+declare -a board=(" " " " " " " " " " " " " " " " ")
+
+function print_board() {
+    echo " ${board[0]} | ${board[1]} | ${board[2]} "
+    echo "---|---|---"
+    echo " ${board[3]} | ${board[4]} | ${board[5]} "
+    echo "---|---|---"
+    echo " ${board[6]} | ${board[7]} | ${board[8]} "
+}
